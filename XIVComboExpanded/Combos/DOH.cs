f7143a47b838f7138072437f56d7b38972edda9e
ZachESN <88117906+ZachESN@users.noreply.github.com>
2022-01-22 18:22:03 -0500

    Update DOH.cs
    i did some DOH prep work to kill some time. i havent figured out how to implement quality yet but i got all the icons id's, and wrote up some of the basic code it would need. it'll prolly also need to have different id's saved per class ( the same way the DOL does) so the only id's i grabbed for testing were for carpenter.

--- XIVComboExpanded/Combos/DOH.cs
+++ XIVComboExpanded/Combos/DOH.cs
@@ -6,12 +6,33 @@
         public const byte JobID = 50;
 
         public const uint
-            Placeholder = 0;
+            //Combos
+            CrpBasicTouch = 1502,
+            CrpStandardTouch = 1516,
+            CrpAdvancedTouch = 1519,
+            CrpPreciseTouch = 1524,
+            CrpIntenseSynthesis = 1514,
+            // Focused things
+            Observe = 1954,
+            CrpFocusedSynthesis = 1536,
+            CrpFocusedTouch = 1535;
 
         public static class Buffs
         {
             public const ushort
-                Placeholder = 0;
+                InnerQuiet = 0,
+                GreatStrides = 0,
+                Innovation = 0,
+                Veneration = 0;
+        }
+        
+        public static class Quality
+        {
+            public const ??
+                Good = 0,
+                Normal = 0,
+                Excelent = 0,
+                Poor = 0;
         }
 
         public static class Debuffs
@@ -23,7 +44,63 @@
         public static class Levels
         {
             public const byte
-                Placeholder = 0;
+                BasicTouch = 5,
+                Observe = 13,
+                Veneration = 15,
+                StandardTouch = 18,
+                GreatStrides = 21,
+                Innovation = 26,
+                PreciseTouch = 53,
+                FocusedSynthesis = 67,
+                FocusedTouch = 68,
+                IntenseSynthesis = 78,
+                AdvancedTouch = 84;
         }
     }
+     
+    internal class CarpenterAdvancedTouch : CustomCombo
+    {
+        protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.DohTouchCombo;
+
+        protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
+        {
+            if (actionID == DOH.AdvancedTouch)
+            {
+                //if (level >= DOH.Levels.PreciseTouch && HasEffect(DOH.Quality.???) && IsEnabled(CustomComboPreset.QualityOverrideFeature))
+                //  return DOH.PreciseTouch;
+                    
+                if (comboTime > 0)
+                {
+                    if (lastComboMove == DOH.StandardTouch && level >= DOH.Levels.AdvancedTouch)
+                        return DOH.AdvancedTouch;
+
+                    if (lastComboMove == DOH.BasicTouch && level >= SAM.Levels.Jinpu)
+                        return DOH.StandardTouch;
+                }
+                    return DOH.StandardTouch;
+
+                return DOH.BasicTouch;
+            }
+            return actionID;
+        }
+    }
+    
+    internal class DohBasicSynthesis : CustomCombo
+    {
+        protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.DohSynthesisFeature;
+
+        protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
+        {
+            if (actionID == DOH.BasicSynthesis)
+            {
+                //if (level >= DOH.Levels.IntenseSynthesis && HasEffect(DOH.Quality.???) && IsEnabled(CustomComboPreset.QualityOverrideFeature))
+                //  return DOH.IntenseSynthesis;
+                
+                return DOH.BasicSynthesis;
+            }
+
+            return actionID;
+        }
+    }
+
 }
