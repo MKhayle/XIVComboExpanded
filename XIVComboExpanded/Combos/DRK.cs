a7a1e7d92f7d63c6de4a31b34e349f83414ab26c
sSasha <55117631+SashaFFXIV@users.noreply.github.com>
2022-02-27 15:39:49 +1000

    Add Mana Overcap Protection Logic

--- XIVComboExpanded/Combos/DRK.cs
+++ XIVComboExpanded/Combos/DRK.cs
@@ -72,7 +72,16 @@ internal class DarkHardSlash : CustomCombo
         {
             var gauge = GetJobGauge<DRKGauge>();
 
-            if ((comboTime <= 0) && IsEnabled(CustomComboPreset.DarkGaugeOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponProtection))
+            if ((level >= DRK.Levels.FloodOfDarkness) && IsEnabled(CustomComboPreset.DarkManaOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && HasEffect(DRK.Buffs.BloodWeapon) && LocalPlayer?.CurrentMp >= 9400)
+            {
+                if (level >= DRK.Levels.Shadow)
+                    return DRK.EdgeOfShadow;
+                if (level >= DRK.Levels.EdgeOfDarkness)
+                    return DRK.EdgeOfDarkness;
+                return DRK.FloodOfDarkness;
+            }
+
+            if (lastComboMove != DRK.HardSlash && IsEnabled(CustomComboPreset.DarkGaugeOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponProtection))
             {
                 if (level >= DRK.Levels.Bloodspiller && gauge.Blood > 90 && HasEffect(DRK.Buffs.BloodWeapon))
                 return DRK.Bloodspiller;
@@ -95,10 +104,32 @@ internal class DarkSyphonStrike : CustomCombo
         {
             var gauge = GetJobGauge<DRKGauge>();
 
+            if ((level >= DRK.Levels.FloodOfDarkness) && lastComboMove == DRK.HardSlash && IsEnabled(CustomComboPreset.DarkManaOvercapProtection))
+            {
+                if (IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && HasEffect(DRK.Buffs.BloodWeapon) && LocalPlayer?.CurrentMp >= 8800)
+                {
+                    if (level >= DRK.Levels.Shadow)
+                        return DRK.EdgeOfShadow;
+                    if (level >= DRK.Levels.EdgeOfDarkness)
+                        return DRK.EdgeOfDarkness;
+                    return DRK.FloodOfDarkness;
+                }
+
+                if (LocalPlayer?.CurrentMp >= 9400)
+                {
+                    if (level >= DRK.Levels.Shadow)
+                        return DRK.EdgeOfShadow;
+                    if (level >= DRK.Levels.EdgeOfDarkness)
+                        return DRK.EdgeOfDarkness;
+                    return DRK.FloodOfDarkness;
+                }
+            }
+
             if (lastComboMove == DRK.HardSlash && IsEnabled(CustomComboPreset.DarkGaugeOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponProtection))
             {
-                if (gauge.Blood > 90 && HasEffect(DRK.Buffs.BloodWeapon))
+                if (gauge.Blood > 90 && HasEffect(DRK.Buffs.BloodWeapon) && (level >= DRK.Levels.Bloodspiller))
                 return DRK.Bloodspiller;
+
             }
 
             return DRK.SyphonStrike;
@@ -117,6 +148,18 @@ internal class DarkSouleater : CustomCombo
         if (actionID == DRK.Souleater)
         {
             var gauge = GetJobGauge<DRKGauge>();
+
+            if (lastComboMove == DRK.SyphonStrike && IsEnabled(CustomComboPreset.DarkManaOvercapProtection))
+            {
+                if ((level >= DRK.Levels.FloodOfDarkness) && IsEnabled(CustomComboPreset.DarkManaOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && HasEffect(DRK.Buffs.BloodWeapon) && LocalPlayer?.CurrentMp >= 9400)
+                {
+                    if (level >= DRK.Levels.Shadow)
+                        return DRK.EdgeOfShadow;
+                    if (level >= DRK.Levels.EdgeOfDarkness)
+                        return DRK.EdgeOfDarkness;
+                    return DRK.FloodOfDarkness;
+                }
+            }
 
             if (IsEnabled(CustomComboPreset.DarkDeliriumFeature))
             {
@@ -146,11 +189,73 @@ internal class DarkSouleater : CustomCombo
                     }
 
                     if (lastComboMove == DRK.HardSlash && level >= DRK.Levels.SyphonStrike)
+                    {
+                        if ((level >= DRK.Levels.FloodOfDarkness) && IsEnabled(CustomComboPreset.DarkManaOvercapProtection))
+                        {
+                            if (IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && HasEffect(DRK.Buffs.BloodWeapon) && LocalPlayer?.CurrentMp >= 8800)
+                            {
+                                if (level >= DRK.Levels.Shadow)
+                                    return DRK.EdgeOfShadow;
+                                if (level >= DRK.Levels.EdgeOfDarkness)
+                                    return DRK.EdgeOfDarkness;
+                                return DRK.FloodOfDarkness;
+                            }
+
+                            if (LocalPlayer?.CurrentMp >= 9400)
+                            {
+                                if (level >= DRK.Levels.Shadow)
+                                    return DRK.EdgeOfShadow;
+                                if (level >= DRK.Levels.EdgeOfDarkness)
+                                    return DRK.EdgeOfDarkness;
+                                return DRK.FloodOfDarkness;
+                            }
+                        }
+
                         return DRK.SyphonStrike;
+                    }
+                }
+
+                if ((level >= DRK.Levels.FloodOfDarkness) && IsEnabled(CustomComboPreset.DarkManaOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && HasEffect(DRK.Buffs.BloodWeapon) && LocalPlayer?.CurrentMp >= 9400)
+                {
+                    if (level >= DRK.Levels.Shadow)
+                        return DRK.EdgeOfShadow;
+                    if (level >= DRK.Levels.EdgeOfDarkness)
+                        return DRK.EdgeOfDarkness;
+                    return DRK.FloodOfDarkness;
                 }
 
                 return DRK.HardSlash;
             }
+        }
+
+        return actionID;
+    }
+}
+
+internal class DarkUnleash : CustomCombo
+{
+    protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.DrkAny;
+
+    protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
+    {
+        if (actionID == DRK.Unleash)
+        {
+            var gauge = GetJobGauge<DRKGauge>();
+
+            if ((level >= DRK.Levels.FloodOfDarkness) && IsEnabled(CustomComboPreset.DarkManaOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && HasEffect(DRK.Buffs.BloodWeapon) && LocalPlayer?.CurrentMp >= 9400)
+            {
+                if (level >= DRK.Levels.Shadow)
+                    return DRK.FloodOfShadow;
+                return DRK.FloodOfDarkness;
+            }
+
+            if (lastComboMove != DRK.Unleash && IsEnabled(CustomComboPreset.DarkGaugeOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponProtection))
+            {
+                if (level >= DRK.Levels.Quietus && gauge.Blood > 90 && HasEffect(DRK.Buffs.BloodWeapon))
+                    return DRK.Quietus;
+            }
+
+            return DRK.Unleash;
         }
 
         return actionID;
@@ -167,6 +272,16 @@ internal class DarkStalwartSoul : CustomCombo
         {
             var gauge = GetJobGauge<DRKGauge>();
 
+            if (lastComboMove == DRK.Unleash && IsEnabled(CustomComboPreset.DarkManaOvercapProtection))
+            {
+                if ((level >= DRK.Levels.FloodOfDarkness) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && LocalPlayer?.CurrentMp >= 9400)
+                {
+                    if (level >= DRK.Levels.Shadow)
+                        return DRK.FloodOfShadow;
+                    return DRK.FloodOfDarkness;
+                }
+            }
+
             if (IsEnabled(CustomComboPreset.DarkDeliriumFeature))
             {
                 if (level >= DRK.Levels.Quietus && level >= DRK.Levels.Delirium && HasEffect(DRK.Buffs.Delirium))
@@ -175,24 +290,36 @@ internal class DarkStalwartSoul : CustomCombo
 
             if (lastComboMove == DRK.Unleash && IsEnabled(CustomComboPreset.DarkGaugeOvercapProtection))
             {
+
+                if ((level >= DRK.Levels.FloodOfDarkness) && IsEnabled(CustomComboPreset.DarkManaOvercapProtection) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && HasEffect(DRK.Buffs.BloodWeapon) && LocalPlayer?.CurrentMp >= 9400)
+                {
+                    if (level >= DRK.Levels.Shadow)
+                        return DRK.FloodOfShadow;
+                    return DRK.FloodOfDarkness;
+                }
+
+                if (level >= DRK.Levels.Quietus && gauge.Blood > 90 && HasEffect(DRK.Buffs.BloodWeapon) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponProtection))
+                    return DRK.Quietus;
+
                 if (level >= DRK.Levels.Quietus && (gauge.Blood > 80 || (gauge.Blood > 70 && HasEffect(DRK.Buffs.BloodWeapon) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponProtection))))
                     return DRK.Quietus;
             }
 
             if (IsEnabled(CustomComboPreset.DarkStalwartSoulCombo))
             {
-                if (IsEnabled(CustomComboPreset.DarkGaugeOvercapProtection))
-                {
-                    if (level >= DRK.Levels.Quietus && gauge.Blood > 90 && HasEffect(DRK.Buffs.BloodWeapon) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponProtection))
-                        return DRK.Quietus;
-                }
-
                 if (comboTime > 0)
                 {
                     if (lastComboMove == DRK.Unleash && level >= DRK.Levels.StalwartSoul)
                     {
                         return DRK.StalwartSoul;
                     }
+                }
+
+                if ((level >= DRK.Levels.FloodOfDarkness) && IsEnabled(CustomComboPreset.DarkIncludeBloodWeaponManaProtection) && HasEffect(DRK.Buffs.BloodWeapon) && LocalPlayer?.CurrentMp >= 9400)
+                {
+                    if (level >= DRK.Levels.Shadow)
+                        return DRK.FloodOfShadow;
+                    return DRK.FloodOfDarkness;
                 }
 
                 return DRK.Unleash;
