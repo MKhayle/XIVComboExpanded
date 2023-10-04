3d4561fa3489fe414026dde64402577b85a01879
Dranni <dranni21312@gmail.com>
2023-10-03 22:20:23 -0500

    6.5 / api9

--- XIVComboExpanded/XIVComboExpandedPlugin.cs
+++ XIVComboExpanded/XIVComboExpandedPlugin.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Linq;
-
+using Dalamud.Game;
 using Dalamud.Game.Command;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
 using XIVComboExpandedPlugin.Interface;
 
 namespace XIVComboExpandedPlugin;
@@ -22,19 +23,20 @@ public sealed partial class XIVComboExpandedPlugin : IDalamudPlugin
     /// Initializes a new instance of the <see cref="XIVComboExpandedPlugin"/> class.
     /// </summary>
     /// <param name="pluginInterface">Dalamud plugin interface.</param>
-    public XIVComboExpandedPlugin(DalamudPluginInterface pluginInterface)
+    /// <param name="gameInteropProvider">Dalamud game interop provider</param>
+    public XIVComboExpandedPlugin(DalamudPluginInterface pluginInterface, IGameInteropProvider gameInteropProvider)
     {
         pluginInterface.Create<Service>();
 
         Service.Configuration = pluginInterface.GetPluginConfig() as PluginConfiguration ?? new PluginConfiguration();
         Service.Address = new PluginAddressResolver();
-        Service.Address.Setup();
+        Service.Address.Setup(new SigScanner());
 
         if (Service.Configuration.Version == 4)
             this.UpgradeConfig4();
 
         Service.ComboCache = new CustomComboCache();
-        Service.IconReplacer = new IconReplacer();
+        Service.IconReplacer = new IconReplacer(gameInteropProvider);
 
         this.configWindow = new();
         this.windowSystem = new("XIVComboExpanded");
